e061b0af8f26f85a8c473ee7ec59614933219ace
Adriel Pereira <adriel.pemi@gmail>
2023-02-23 09:48:49 -0400

    Add project files.

---
+++ WebCRUDMVCSQL/Models/Contexto.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebCRUDMVCSQL.Models
+{
+    public class Contexto : DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> options):base(options)
+        {
+        }
+        public DbSet<Produto> Produto { get; set; }
+
+    }
+}
